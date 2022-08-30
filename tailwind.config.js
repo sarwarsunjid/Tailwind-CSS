4d8c032f138109bc272c3d390c108294bb02dc26
Sarwar Jahan Bhuiyan <sarwarsunjid@gmail.com>
2022-08-30 18:31:27 +0600

    adding google fonts and theme configuration including fonts, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
